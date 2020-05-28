71181b31aeff695cfdf2ff9e3dd2ece4e5560d4d
Tony <tonyhood@live.com>
2020-05-28 08:20:41 -0400

    Corrected Issues
    RankedGradeBook now inherits from BaseGradeBook
    
    Corrected Property getter and setter in BaseGradeBook for type.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : StandardGradeBook
+    public class RankedGradeBook :BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
